8eb689e0f610eb5304677f9a54396b295f043a82
tylerhindman <tylerhindman64@gmail.com>
2023-07-15 16:05:48 -0500

    Shot my sling 2

--- Assets/[Scripts]/[Slingshot]/Slingshot.cs
+++ Assets/[Scripts]/[Slingshot]/Slingshot.cs
@@ -6,26 +6,26 @@ using UnityEngine;
 [Serializable]
 public class Slingshot : MonoBehaviour
 {
-    public GameObject target;
+    public GameObject diceTarget;
+    public float lineAnchorOffset;
     public float lineMaxLength;
     public float lineMouseSlingshotScale;
 
     private Vector3 mouseRotReference;
     // Start is called before the first frame update
     private LineRenderer lineRenderer;
+    private Camera mainCamera;
     void Start()
     {
         this.lineRenderer = this.GetComponent<LineRenderer>();
         this.lineRenderer.enabled = false;
+
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) {
-            this.lineRenderer.SetPosition(0, new Vector3(this.lineRenderer.GetPosition(0).x, this.lineRenderer.GetPosition(0).y, this.lineRenderer.GetPosition(0).z - 0.1f));
-        }
-
         // Grab mouse reference point on left click
         if (Input.GetMouseButtonDown(0)) {
             this.mouseRotReference = Input.mousePosition;
@@ -40,12 +40,18 @@ public class Slingshot : MonoBehaviour
                 if (!this.lineRenderer.enabled) {
                     this.lineRenderer.enabled = true;
                 }
-                var newArrowAngle = Mathf.Rad2Deg * Mathf.Atan2(mouseDiff.normalized.y, mouseDiff.normalized.x);
-                transform.rotation = Quaternion.AngleAxis(newArrowAngle - 90f, Vector3.up);
-                transform.position = target.transform.position + Quaternion.AngleAxis(newArrowAngle, Vector3.up) * new Vector3(1.5f, 0, 0);
+                var newArrowAngle = Mathf.Rad2Deg * Mathf.Atan2(mainCamera.transform.forward.x, mainCamera.transform.forward.z);
+                var mouseAngle = Mathf.Rad2Deg * Mathf.Atan2(mouseDiff.normalized.y, mouseDiff.normalized.x);
+                //newArrowAngle -= cameraForwardAngleFromTarget;
 
-                var lineLength = Mathf.Clamp((-mouseDiff.magnitude * this.lineMouseSlingshotScale) + this.lineRenderer.GetPosition(1).z, -this.lineMaxLength, this.lineRenderer.GetPosition(1).z);
+                transform.rotation = Quaternion.AngleAxis(newArrowAngle - 180f, Vector3.up);
+                transform.position = this.diceTarget.transform.position + Quaternion.AngleAxis(newArrowAngle - 90f, Vector3.up) * new Vector3(this.lineAnchorOffset, 0, 0);
+
+                var lineLength =
+                    Mathf.Clamp((Mathf.Sign(mouseAngle) * mouseDiff.magnitude * this.lineMouseSlingshotScale) + this.lineRenderer.GetPosition(1).z, -this.lineMaxLength, this.lineRenderer.GetPosition(1).z);
                 this.lineRenderer.SetPosition(0, new Vector3(this.lineRenderer.GetPosition(0).x, this.lineRenderer.GetPosition(0).y, lineLength));
+
+                Debug.Log("mouseAngle: " + mouseAngle);
             }
         }
 
