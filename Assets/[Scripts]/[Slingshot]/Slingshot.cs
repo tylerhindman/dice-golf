4ebbdd7598e71642c99f096b6594f6a312095366
tylerhindman <tylerhindman64@gmail.com>
2023-07-15 16:41:13 -0500

    Sling shot? Sling DONE (visually)

--- Assets/[Scripts]/[Slingshot]/Slingshot.cs
+++ Assets/[Scripts]/[Slingshot]/Slingshot.cs
@@ -10,6 +10,7 @@ public class Slingshot : MonoBehaviour
     public float lineAnchorOffset;
     public float lineMaxLength;
     public float lineMouseSlingshotScale;
+    public float maxMouseY;
 
     private Vector3 mouseRotReference;
     // Start is called before the first frame update
@@ -41,17 +42,17 @@ public class Slingshot : MonoBehaviour
                     this.lineRenderer.enabled = true;
                 }
                 var newArrowAngle = Mathf.Rad2Deg * Mathf.Atan2(mainCamera.transform.forward.x, mainCamera.transform.forward.z);
-                var mouseAngle = Mathf.Rad2Deg * Mathf.Atan2(mouseDiff.normalized.y, mouseDiff.normalized.x);
+                //var newArrowAngle = Mathf.Rad2Deg * Mathf.Atan2(mouseDiff.normalized.y, mouseDiff.normalized.x);
                 //newArrowAngle -= cameraForwardAngleFromTarget;
 
                 transform.rotation = Quaternion.AngleAxis(newArrowAngle - 180f, Vector3.up);
                 transform.position = this.diceTarget.transform.position + Quaternion.AngleAxis(newArrowAngle - 90f, Vector3.up) * new Vector3(this.lineAnchorOffset, 0, 0);
 
-                var lineLength =
-                    Mathf.Clamp((Mathf.Sign(mouseAngle) * mouseDiff.magnitude * this.lineMouseSlingshotScale) + this.lineRenderer.GetPosition(1).z, -this.lineMaxLength, this.lineRenderer.GetPosition(1).z);
+                var lineLength = Mathf.Lerp(this.lineRenderer.GetPosition(1).z, -this.lineMaxLength, -y / this.maxMouseY);
+                //var lineLength = Mathf.Clamp((-mouseDiff.magnitude * this.lineMouseSlingshotScale) + this.lineRenderer.GetPosition(1).z, -this.lineMaxLength, this.lineRenderer.GetPosition(1).z);
                 this.lineRenderer.SetPosition(0, new Vector3(this.lineRenderer.GetPosition(0).x, this.lineRenderer.GetPosition(0).y, lineLength));
 
-                Debug.Log("mouseAngle: " + mouseAngle);
+                Debug.Log("mouseY: " + y + "percentage: " + -y / this.maxMouseY);
             }
         }
 
