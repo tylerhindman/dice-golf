3ecfbb908fb69d34ce4afe39fd8cd6e1c9a8add3
tylerhindman <tylerhindman64@gmail.com>
2023-07-15 22:52:26 -0500

    New input system (Thanks N64 NPC)

--- Assets/[Scripts]/[Slingshot]/Slingshot.cs
+++ Assets/[Scripts]/[Slingshot]/Slingshot.cs
@@ -11,7 +11,7 @@ public class Slingshot : MonoBehaviour
     public float forceMultiplier;
     public float forceHeightOffset;
 
-    private Vector3 mouseRotReference;
+    private float mouseYReference;
     // Start is called before the first frame update
     private LineRenderer lineRenderer;
     private Camera mainCamera;
@@ -29,17 +29,16 @@ public class Slingshot : MonoBehaviour
         // Mouse down trigger
         if (Input.GetMouseButtonDown(0)) {
             // Grab mouse reference point on left click
-            this.mouseRotReference = Input.mousePosition;
+            this.mouseYReference = 0;
         }
 
         // Mouse held
         if (Input.GetMouseButton(0)) {
             // Get mouse diff vector vals
-            var x = -(Input.mousePosition.x - this.mouseRotReference.x);
-            var y = (Input.mousePosition.y - this.mouseRotReference.y);
-            var mouseDiff = new Vector3(x, y, 0);
+            var mouseY = Input.GetAxis("Mouse Y");
+            this.mouseYReference += mouseY;
             // Only update slingshot if mouse moved from reference
-            if (y < 0f) {
+            if (this.mouseYReference < 0f) {
                 // Enable line renderer first time
                 if (!this.lineRenderer.enabled) {
                     this.lineRenderer.enabled = true;
@@ -52,7 +51,7 @@ public class Slingshot : MonoBehaviour
                 transform.position = this.diceTarget.transform.position + Quaternion.AngleAxis(newArrowAngle - 90f, Vector3.up) * new Vector3(this.lineAnchorOffset, 0, 0);
 
                 // Update length of slingshot based on mouse y
-                var lineLength = Mathf.Lerp(this.lineRenderer.GetPosition(1).z, -this.lineMaxLength, -y / this.maxMouseY);
+                var lineLength = Mathf.Lerp(this.lineRenderer.GetPosition(1).z, -this.lineMaxLength, -this.mouseYReference / this.maxMouseY);
                 this.lineRenderer.SetPosition(0, new Vector3(this.lineRenderer.GetPosition(0).x, this.lineRenderer.GetPosition(0).y, lineLength));
             }
         }
@@ -63,12 +62,10 @@ public class Slingshot : MonoBehaviour
             this.lineRenderer.enabled = false;
 
             // Get mouse diff vector vals
-            var x = -(Input.mousePosition.x - this.mouseRotReference.x);
-            var y = (Input.mousePosition.y - this.mouseRotReference.y);
-            var mouseDiff = new Vector3(x, y, 0);
+            var mouseY = Input.GetAxis("Mouse Y");
             // Apply force based on slingshot magnitude
-            if (y < 0f) {
-                this.diceTarget.GetComponent<Rigidbody>().AddForceAtPosition(this.mainCamera.transform.forward * Mathf.Clamp(-y / this.maxMouseY, 0f, 1f) * this.forceMultiplier,
+            if (this.mouseYReference < 0f) {
+                this.diceTarget.GetComponent<Rigidbody>().AddForceAtPosition(this.mainCamera.transform.forward * Mathf.Clamp(-this.mouseYReference / this.maxMouseY, 0f, 1f) * this.forceMultiplier,
                     new Vector3(this.diceTarget.transform.position.x, this.diceTarget.transform.position.y + this.forceHeightOffset, this.diceTarget.transform.position.z),
                     ForceMode.Impulse);
             }
