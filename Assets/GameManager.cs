3def6518681eff3f2f39760a01bb83978df430d2
Ian Guglielmi-White <ianrgwhite@gmail.com>
2023-07-16 02:55:13 -0500

    Dice have UI sprite images (PLACEHOLDERS)

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -11,9 +12,20 @@ public class GameManager : MonoBehaviour
 
     // UI hookups
     [Header("UI Hookups")]
+    [SerializeField] Image UICurrentDieImage;
     [SerializeField] TMP_Text UICurrentDie;
     [SerializeField] TMP_Text UIScore;
     [SerializeField] TMP_Text UIRollCount;
+
+    // UI Die Sprites
+    [Header("UI Die Sprites")]
+    [SerializeField] Sprite spriteD4;
+    [SerializeField] Sprite spriteD6;
+    [SerializeField] Sprite spriteD6b;
+    [SerializeField] Sprite spriteD8;
+    [SerializeField] Sprite spriteD10;
+    [SerializeField] Sprite spriteD12;
+    [SerializeField] Sprite spriteD20;
     
     void Awake()
     {
@@ -29,6 +41,7 @@ public class GameManager : MonoBehaviour
         Rolls = 0;
 
         //Set Default UI
+        UICurrentDieImage.sprite = spriteD6;
         UICurrentDie.text = "Equipped: d6";
     }
 
@@ -74,16 +87,69 @@ public class GameManager : MonoBehaviour
 
     public void UIDieSwap(int dieIndex)
     {
-        if (dieIndex == -1) UICurrentDie.text = "Equipped: d6";
-        if (dieIndex == 0) UICurrentDie.text = "Equipped: d4";
-        if (dieIndex == 1) UICurrentDie.text = "Equipped: d6";
-        if (dieIndex == 2) UICurrentDie.text = "Equipped: d6b";
-        if (dieIndex == 3) UICurrentDie.text = "Equipped: d8";
-        if (dieIndex == 4) UICurrentDie.text = "Equipped: d10";
-        if (dieIndex == 5) UICurrentDie.text = "Equipped: d12";
-        if (dieIndex == 6) UICurrentDie.text = "Equipped: d20";
+        //D6 is the default starting position
 
+        if (dieIndex == -1)
+        {
+            UICurrentDie.text = "Equipped: d6";
+            UICurrentDieImage.sprite = spriteD6;
+        }
 
+        //D4
+
+        if (dieIndex == 0)
+        {
+            UICurrentDie.text = "Equipped: d4";
+            UICurrentDieImage.sprite = spriteD4;
+        }
+
+        //D6
+
+        if (dieIndex == 1)
+        {
+            UICurrentDie.text = "Equipped: d6";
+            UICurrentDieImage.sprite = spriteD6;
+        }
+
+        //D6  (beveled)
+
+        if (dieIndex == 2)
+        {
+            UICurrentDie.text = "Equipped: d6b";
+            UICurrentDieImage.sprite = spriteD6b;
+        }
+
+        //D8
+
+        if (dieIndex == 3)
+        {
+            UICurrentDie.text = "Equipped: d8";
+            UICurrentDieImage.sprite = spriteD8;
+        }
+
+        //D10
+
+        if (dieIndex == 4)
+        {
+            UICurrentDie.text = "Equipped: d10";
+            UICurrentDieImage.sprite = spriteD10;
+        }
+
+        //D12
+
+        if (dieIndex == 5)
+        {
+            UICurrentDie.text = "Equipped: d12";
+            UICurrentDieImage.sprite = spriteD12;
+        }
+
+        //D20
+
+        if (dieIndex == 6)
+        {
+            UICurrentDie.text = "Equipped: d20";
+            UICurrentDieImage.sprite = spriteD20;
+        }
 
     }
 }
