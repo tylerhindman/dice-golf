b23eb1368768995e106fa83d298df8ef9763a31d
Mikey <mikeybess27@gmail.com>
2023-07-21 15:47:41 -0500

    Dice patterns -- Ready to roll >:3

--- Assets/[Scripts]/[Dice]/RayDice.cs
+++ Assets/[Scripts]/[Dice]/RayDice.cs
@@ -18,17 +18,35 @@ public class RayDice : MonoBehaviour
     protected int _diceValue;
     public int DiceValue => _diceValue;
 
+    // Resolve Actions
     [SerializeField] ParticleSystem _resolveParticles;
     [SerializeField] GameObject _resolvePopup;
+
+    // Audio
     //[SerializeField] AudioClip _diceResolveSFX;
     //[SerializeField] AudioClip _diceThrowSFX;
     //[SerializeField] AudioClip _diceHitSFX;
 
+    // FOR MATH, FACES, AND RAYCASTS
     private Mesh mesh;
     private Vector3[] vertices;
     private int[] triangles;
 
+    // To vizualize location of faces and spawn the plane per face
     [SerializeField] private GameObject sphere;
+
+    // Dice Type Enum
+    private enum DiceType
+    {
+        D4,
+        D6,
+        D8,
+        D10,
+        D12,
+        D20
+    }
+
+    DiceType diceType;
 
     private void Awake()
     {
@@ -55,15 +73,54 @@ public class RayDice : MonoBehaviour
         // Extract vertices and triangles from the mesh
         vertices = mesh.vertices;
         triangles = mesh.triangles;
+
+        switch(vertices.Length)
+        {
+            case 12:
+                diceType = DiceType.D4;
+                break;
+
+            case 24:
+                diceType = DiceType.D6;
+                break;
+
+            case 22:
+                diceType = DiceType.D8;
+                break;
+
+            case 40:
+                diceType = DiceType.D10;
+                break;
+
+            case 60:
+                if(triangles.Length == 108)
+                {
+                    diceType = DiceType.D12;
+                }
+                else if(triangles.Length == 60)
+                {
+                    diceType = DiceType.D20;
+                }
+                break;
+        }
+
+        //Debug.Log(gameObject.name + "   " + diceType);
+        //Debug.Log(gameObject.name + "   " + vertices.Length);
+        //Debug.Log(gameObject.name + "   " + triangles.Length);
+    }
+
+    private void Start()
+    {
+        //RollDice();
     }
 
     private void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.R))
+        /*if (Input.GetKeyDown(KeyCode.R))
         {
             RollDice();
-        }
+        }*/
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -97,8 +154,8 @@ public class RayDice : MonoBehaviour
             _thrown = true;
             _rb.isKinematic = false;
             _rb.useGravity = true;
-            _rb.AddTorque(UnityEngine.Random.Range(0, 50), UnityEngine.Random.Range(0, 50), UnityEngine.Random.Range(0, 50));
-            _rb.AddForce(UnityEngine.Random.Range(-25, 25), 0, UnityEngine.Random.Range(10, 25));
+            //_rb.AddTorque(UnityEngine.Random.Range(0, 50), UnityEngine.Random.Range(0, 50), UnityEngine.Random.Range(0, 50));
+            //_rb.AddForce(UnityEngine.Random.Range(-25, 25), 0, UnityEngine.Random.Range(10, 25));
 
             //AudioManager.Instance.PlaySound2D(_diceThrowSFX, .5f, UnityEngine.Random.Range(.8f, 1.2f));
         }
@@ -184,7 +241,7 @@ public class RayDice : MonoBehaviour
             //Debug.Log(vertices.Length);
 
             // D6 and D10 because they have 2 triangles per face
-            if (triangles.Length == 36 || (triangles.Length == 60 && vertices.Length == 40))
+            if (diceType == DiceType.D6 || diceType == DiceType.D10)
             {
                 // Get the vertices of the current face
                 Vector3 v0 = transform.TransformPoint(vertices[triangles[i]]);
@@ -222,12 +279,12 @@ public class RayDice : MonoBehaviour
 
                             if (i == 0)
                             {
-                                Debug.Log("i: " + i + " --- " + "index: " + 1);
+                                //Debug.Log("i: " + i + " --- " + "index: " + 1);
                                 shortestDistanceIndex = 1; // Store the index of the raycast
                             }
                             else
                             {
-                                Debug.Log("i: " + i + " --- " + "index: " + (float)((i / 6f) + 1));
+                                //Debug.Log("i: " + i + " --- " + "index: " + (float)((i / 6f) + 1));
                                 shortestDistanceIndex = (int)((i / 6f) + 1); // Store the index of the raycast
                             }
                         }
@@ -236,7 +293,7 @@ public class RayDice : MonoBehaviour
                 i += 3;
             }
             // D12 because it has 3 triangles per face
-            else if (triangles.Length == 108)
+            else if (diceType == DiceType.D12)
             {
                 // Get the vertices of the current face
                 Vector3 v0 = transform.TransformPoint(vertices[triangles[i]]);
@@ -275,12 +332,12 @@ public class RayDice : MonoBehaviour
                             shortestDistance = distanceToGround;
                             if (i == 0)
                             {
-                                Debug.Log("i: " + i + " --- " + "index: " + 1);
+                                //Debug.Log("i: " + i + " --- " + "index: " + 1);
                                 shortestDistanceIndex = 1; // Store the index of the raycast
                             }
                             else
                             {
-                                Debug.Log("i: " + i + " --- " + "index: " + (float)((i / 9f) + 1));
+                                //Debug.Log("i: " + i + " --- " + "index: " + (float)((i / 9f) + 1));
                                 shortestDistanceIndex = (int)((i / 9f) + 1); // Store the index of the raycast
                             }
                         }
@@ -322,12 +379,12 @@ public class RayDice : MonoBehaviour
 
                             if (i == 0)
                             {
-                                Debug.Log("i: " + i + " --- " + "index: " + 1);
+                                //Debug.Log("i: " + i + " --- " + "index: " + 1);
                                 shortestDistanceIndex = 1; // Store the index of the raycast
                             }
                             else
                             {
-                                Debug.Log("i: " + i + " --- " + "index: " + (float)((i / 3f) + 1));
+                                //Debug.Log("i: " + i + " --- " + "index: " + (float)((i / 3f) + 1));
                                 shortestDistanceIndex = ((i / 3) + 1); // Store the index of the raycast
                             }
                             
@@ -338,7 +395,43 @@ public class RayDice : MonoBehaviour
             }
         }
         
-        return shortestDistanceIndex;
+        return AttachPattern(shortestDistanceIndex);
+
+    }
+
+    // Commented pattern is the pattern that the faces are created in unity
+    // Pattern is flipped to return the opposite side -- If the 1 on a d6 is on the ground (What i'm checking for) it'll return a 6 because that's what's facing up
+    private int AttachPattern(int num)
+    {
+        
+        switch (diceType)
+        {
+            case DiceType.D4:
+                var d4 = new int[] { 4, 3, 2, 1 };
+                return d4[num-1]; 
+            case DiceType.D6:
+                //var d6 = new int[] { 2, 3, 4, 5, 6, 1 };
+                var d6 = new int[] { 5, 4, 3, 2, 1, 6 };
+                return d6[num - 1];
+            case DiceType.D8:
+                //var d8 = new int[] { 3, 6, 8, 7, 5, 2, 1, 4 };
+                var d8 = new int[] { 6, 3, 1, 2, 4, 7, 8, 5 };
+                return d8[num - 1];
+            case DiceType.D10:
+                //var d10 = new int[] { 0, 8, 3, 5, 2, 6, 9, 1, 4, 7 };
+                var d10 = new int[] { 9, 1, 6, 4, 7, 3, 0, 8, 5, 2 };
+                return d10[num - 1];
+            case DiceType.D12:
+                //var d12 = new int[] { 1, 2, 4, 3, 8, 6, 5, 10, 7, 9, 11, 12 };
+                var d12 = new int[] { 12, 11, 9, 10, 5, 7, 8, 3, 6, 4, 2, 1 };
+                return d12[num - 1];
+            case DiceType.D20:
+                //var d20 = new int[] { 1, 7, 19, 3, 17, 15, 13, 9, 16, 10, 5, 11, 6, 8, 12, 18, 4, 14, 20, 2 };
+                var d20 = new int[] { 20, 14, 2, 18, 4, 6, 8, 12, 5, 11, 16, 10, 15, 13, 9, 3, 17, 7, 1, 19 };
+                return d20[num - 1];
+            default:
+                return -1;
+        }
 
     }
 }
